7eb3f0358b57d3d381e55cb90ebf237dd7b3ae99
Aleyna Elif ÖZKAN <51717390+aleynaelif@users.noreply.github.com>
2023-12-03 20:24:00 +0300

    Final Commit

--- StoreDiscount/src/main/java/org/example/Main.java
+++ StoreDiscount/src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
 
     public static void main(String[] args) {
         // main is empty. Creating objects and testing the program can be handled in testing files.
+        System.out.println("Please run the test file.");
     }
 }
